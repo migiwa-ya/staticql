3e399f588aafbe3e814f56ab4a0783d1614a8cc3
migiwa-ya <contact@migiwa-ya.com>
2025-04-30 02:31:21 +0900

    fix: parse date

--- src/utils.ts
+++ src/utils.ts
@@ -345,7 +345,7 @@ export function parseValue(val: string): any {
     return Number(trimmed);
   }
 
-  if (/^\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}:\d{2}(?:\.\d+)?Z?)?$/.test(trimmed)) {
+  if (/^\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}:\d{2}(?:\.\d+)?Z?)?/.test(trimmed)) {
     const d = new Date(trimmed);
     if (!isNaN(d.getTime())) {
       return d;
